13d50bdeefbc8774bcace7fde6159fc22c5a2252
aiyaya1993 <15958116062@163.com>
2017-09-21 20:02:56 +0800

    集合的学习 数组的方法要多学习

--- Chapter9/Set.js
+++ Chapter9/Set.js
@@ -2,6 +2,7 @@
 function Set() {
 	this.dataStore = []; //数据集
 	this.add = add; //添加方法
+	this.remove = remove; //删除方法
 }
 //添加方法
 function add(ele) {
@@ -9,6 +10,15 @@ function add(ele) {
 		this.dataStore.push(ele);
 		return true;
 	}else {
+		return false;
+	}
+}
+function remove(ele) {
+	var pos = this.dataStore.indexOf(data); //indexof返回某元素在数组中首次出现的位置
+	if(pos>-1) { //存在
+		this.dataStore.splice(pos,1);
+		return true;
+	} else{
 		return false;
 	}
 }
