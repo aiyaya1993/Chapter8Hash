508eb2c6752989aa6e76bb8da6f9d7e090320f62
aiyaya1993 <15958116062@163.com>
2017-09-21 20:06:47 +0800

    集合的学习

--- Chapter9/Set.js
+++ Chapter9/Set.js
@@ -3,6 +3,7 @@ function Set() {
 	this.dataStore = []; //数据集
 	this.add = add; //添加方法
 	this.remove = remove; //删除方法
+	this.show = show;
 }
 //添加方法
 function add(ele) {
@@ -13,6 +14,7 @@ function add(ele) {
 		return false;
 	}
 }
+//删除操作
 function remove(ele) {
 	var pos = this.dataStore.indexOf(data); //indexof返回某元素在数组中首次出现的位置
 	if(pos>-1) { //存在
@@ -22,3 +24,15 @@ function remove(ele) {
 		return false;
 	}
 }
+//展示所有元素
+function show() {
+	//console.log(this.dataStore);
+	for(i in this.dataStore){
+		console.log(i + "  " + this.dataStore[i]);
+	}
+}
+var names = new Set();
+names.add("Tom");
+names.add("Jerry");
+names.add("Mike");
+names.show();
